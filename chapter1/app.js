e9047044808f62b459841816764162fc1f24f278
yogjin <youngjin988@gmail.com>
2022-01-25 23:15:28 +0900

    modify config-code to fit web3@1.7.0

--- chapter1/app.js
+++ chapter1/app.js
@@ -3,9 +3,11 @@ const Web3 = require('web3');
 // ganache와 연결
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 // 각 계정에 100ether씩 보유중인지 확인
-const accounts = web3.eth.accounts;
-accounts.forEach((account) => {
-  const wei = web3.eth.getBalance(account).toNumber();
-  const ether = web3.fromWei(wei, 'ether');
-  console.log(ether);
+const accPromise = web3.eth.getAccounts();
+accPromise.then((accounts) => {
+  accounts.forEach(async (account) => {
+    const wei = await web3.eth.getBalance(account);
+    const ether = web3.utils.fromWei(wei, 'ether');
+    console.log(ether);
+  });
 });
